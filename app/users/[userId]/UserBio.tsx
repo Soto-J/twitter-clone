4cb27032c637e70df3410580f187b0c3f8917c0c
John Soto <soto.ja415@gmail.com>
2023-09-02 03:42:30 -0400

    Adjusted styling

--- app/users/[userId]/UserBio.tsx
+++ app/users/[userId]/UserBio.tsx
@@ -28,27 +28,27 @@ const UserBio = ({ user, currentUser }: UserBioProps) => {
           <Button secondary label="Follow" onClick={() => {}} />
         )}
       </div>
+
       <div className="mt-8 px-4">
         <div className="flex flex-col">
           <h2 className="text-2xl font-semibold text-white">{user?.name}</h2>
           <p className="text-neutral-500">@{user?.username}</p>
-
-          <div className="mt-4 flex flex-col">
-            <p className="text-white">
-              {user?.bio || "This account has no bio."}
-            </p>
-            <div
-              className="
+        </div>
+        <div className="mt-4 flex flex-col">
+          <p className="text-white">
+            {user?.bio || "This account has no bio."}
+          </p>
+          <div
+            className="
                 mt-4
                 flex
                 items-center
                 gap-2
                 text-neutral-500  
               "
-            >
-              <BiCalendar size={24} />
-              <p>Joined {createdAt()}</p>
-            </div>
+          >
+            <BiCalendar size={24} />
+            <p>Joined {createdAt()}</p>
           </div>
         </div>
 
@@ -57,9 +57,6 @@ const UserBio = ({ user, currentUser }: UserBioProps) => {
             <p className="text-white">{user?.followingIds.length}</p>
             <p className="text-neutral-500">Following</p>
           </div>
-        </div>
-
-        <div className="mt-4 flex items-center gap-6">
           <div className="flex items-center gap-1">
             <p className="text-white">{user?.followersCount}</p>
             <p className="text-neutral-500">Followers</p>
