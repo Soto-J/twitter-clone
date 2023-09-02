9933ace7ef557213a4b80a9c0ec5e0134300e264
John Soto <soto.ja415@gmail.com>
2023-09-02 03:15:52 -0400

    Added onClickHandler to mobile view

--- app/components/sidebar/SidebarItem.tsx
+++ app/components/sidebar/SidebarItem.tsx
@@ -37,7 +37,7 @@ const SidebarItem = ({ href, label, icon: Icon, logOut }: SidebarItemProps) => {
     <div className="flex items-center">
       {/* Mobile */}
       <div
-        onClick={logOut}
+        onClick={onClickHandler}
         className="
           relative
           h-14
