ef9f5771565204ba6877b840a0873e6e7dc8d350
John Soto <soto.ja415@gmail.com>
2023-09-15 05:04:30 -0400

    getSerSideProps was throwing an errror

--- app/notifications/page.tsx
+++ app/notifications/page.tsx
@@ -5,22 +5,22 @@ import { getSession } from "next-auth/react";
 
 import NotificationsFeed from "./NotificationsFeed";
 
-export async function getServerSideProps(context: NextPageContext) {
-  const session = await getSession(context);
+// export async function getServerSideProps(context: NextPageContext) {
+//   const session = await getSession(context);
   
-  if (!session) {
-    return {
-      redirect: {
-        destination: "/",
-        permanent: false,
-      },
-    };
-  }
+//   if (!session) {
+//     return {
+//       redirect: {
+//         destination: "/",
+//         permanent: false,
+//       },
+//     };
+//   }
 
-  return {
-    props: { session },
-  };
-}
+//   return {
+//     props: { session },
+//   };
+// }
 
 const page = () => {
   return (
